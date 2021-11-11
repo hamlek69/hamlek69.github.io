cb3c7ab7621930c630f5de98e29588e2150909b8
hamlek69 <66007712+hamlek69@users.noreply.github.com>
2021-11-11 12:34:46 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -15,10 +15,10 @@ var myHeading = document.querySelector('h1');
 // Toggle Source Image
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'pics/wolke.png') {
+    if(mySrc === 'pics/Wolke.png') {
       myImage.setAttribute ('src','pics/Bernd.png');
     } else {
-      myImage.setAttribute ('src','pics/wolke.png');
+      myImage.setAttribute ('src','pics/Wolke.png');
     }
 }
 
